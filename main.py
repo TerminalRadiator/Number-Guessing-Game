c9d4f160e922f490445d40bc33a21dca9f3eea47
TerminalRadiator <61122621+TerminalRadiator@users.noreply.github.com>
2022-03-04 21:14:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import random
 
-num = random.randint(1, 10)
+chance = 5
+num = random.randint(1, 101)
 num_guessed = int(input("Welcome to the Number Guessing Game!!! Please guess a number: "))
 
 
-while num_guessed != num:
-    num_guessed = int(input("Guess again: "))
+while num_guessed != num and chance != 0:
+    chance -= 1
+    if chance == 0:
+        print('You lose!!!')
+    else:
+        print(f"You have {chance} chances left")
+        if num_guessed < num:
+            print("The number is higher")
+        else:
+            print('The number is lower')
+        num_guessed = int(input("Guess again: "))
 
 if num_guessed == num:
     print("You guessed correctly!!!")
